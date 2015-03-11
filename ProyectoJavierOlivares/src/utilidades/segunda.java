b7dd654233658b126377b1f58ba4dd3d56bd5828
Olivares <Olivares@Olivares-PC>
2015-03-11 10:13:27 +0100

    segunda

---
+++ ProyectoJavierOlivares/src/utilidades/segunda.java
@@ -0,0 +1,11 @@
+package utilidades;
+
+public class segunda {
+
+	public static void main(String[] args) {
+		System.out.println("Este es el proyecto de Javier Olivares.");
+		System.out.println("segunda.");
+		
+	}
+
+}
