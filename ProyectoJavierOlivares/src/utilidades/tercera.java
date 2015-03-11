df3ecba6dd2284bd5ae9d066e4b34149e03acfd8
Olivares <Olivares@Olivares-PC>
2015-03-11 10:15:15 +0100

    tercera

---
+++ ProyectoJavierOlivares/src/utilidades/tercera.java
@@ -0,0 +1,12 @@
+package utilidades;
+
+public class tercera {
+
+	public static void main(String[] args) {
+		System.out.println("Este es el proyecto de Javier Olivares.");
+		System.out.println("tercera.");
+
+		
+	}
+
+}
