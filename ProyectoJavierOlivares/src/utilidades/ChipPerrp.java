7270914362b14ae0f1878e36017bc0d763b56b2a
Olivares <Olivares@Olivares-PC>
2015-03-11 10:28:15 +0100

    Funcionalidad chip perro

--- ProyectoJavierOlivares/src/utilidades/ChipPerrp.java
+++ ProyectoJavierOlivares/src/utilidades/ChipPerrp.java
@@ -1,10 +1,10 @@
 package utilidades;
 
-public class ChipPerro {
+public class ChipPerrp {
 
 	public static void main(String[] args) {
+		// TODO Auto-generated method stub
 
-		System.out.println("perro.");
 	}
 
 }
